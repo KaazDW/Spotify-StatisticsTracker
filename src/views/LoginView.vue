46668555f51a3d00adb2c2738119316ee6ff6f23
/kaaz <92046563+KaazDW@users.noreply.github.com>
2023-11-10 14:58:46 +0100

    [dev] homepage integration

---
+++ src/views/LoginView.vue
@@ -0,0 +1,15 @@
+<template>
+    <section class="home">
+      <h5>spotify<i class="bi bi-music-player-fill"></i></h5>
+      <h1>STATS-TRACKER</h1>
+      <img src="/images/mockup_mobile.png" alt="Stats-Tracker mockup" />
+      <div class="bottom">
+        <div class="space"></div>
+        <div class="button-login">
+          <i class="bi bi-spotify" style="color: var(--color-green);font-size: 40px;"></i>
+          <RouterLink class="btn" to="/login">Login<i class="bi bi-arrow-up-right-square-fill" style="color: var(--color-black);"></i></RouterLink>
+        </div>
+      </div>
+     
+    </section>
+</template>
