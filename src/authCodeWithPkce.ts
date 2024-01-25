d968e5c66479cddd3b48d73722645238fdca21a3
/kaaz <92046563+KaazDW@users.noreply.github.com>
2024-01-25 20:08:16 +0100

    [dev] vercel host

--- src/authCodeWithPkce.ts
+++ src/authCodeWithPkce.ts
@@ -7,7 +7,7 @@ export async function redirectToAuthCodeFlow(clientId: string) {
     const params = new URLSearchParams();
     params.append("client_id", clientId);
     params.append("response_type", "code");
-    params.append("redirect_uri", "http://localhost:5173/tracks");
+    params.append("redirect_uri", `${window.location.origin}/tracks`);
     params.append("scope", "user-read-private user-read-email user-top-read user-read-currently-playing");
     params.append("code_challenge_method", "S256");
     params.append("code_challenge", challenge);
