76015b03fe0bd08ca6fb7a66b5762eb0c61cbbd6
/kaaz <92046563+KaazDW@users.noreply.github.com>
2024-01-25 20:02:00 +0100

    [dev] vercel host

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,6 @@ import vue from '@vitejs/plugin-vue';
 // Obtenez l'URL de déploiement de Vercel
 const vercelUrl = process.env.VERCEL_URL || '';
 
-// Affichez l'URL dans la console pendant le développement
-console.log('Vercel URL:', vercelUrl);
-
 export default defineConfig({
   plugins: [vue()],
   resolve: {
