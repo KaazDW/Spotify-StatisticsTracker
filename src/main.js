5ba693f83efce3630f2d4935fd61ecf0f24e1e6e
/kaaz <92046563+KaazDW@users.noreply.github.com>
2024-01-26 09:20:33 +0100

    [host] vercel analytics

--- src/main.js
+++ src/main.js
@@ -5,11 +5,11 @@ import App from './App.vue'
 import router from './router'
 import VercelAnalytics from '@vercel/analytics';
 
-VercelAnalytics({ projectId: 'prj_LIW8SSAt1NTwMQJWEvVExdY1lejp' });
 
 const app = createApp(App)
 
 app.use(router)
 
 app.mount('#app')
+VercelAnalytics({ projectId: 'prj_LIW8SSAt1NTwMQJWEvVExdY1lejp' });
 
