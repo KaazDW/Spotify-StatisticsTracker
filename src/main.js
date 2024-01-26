f1547ad8932dc79f1a1069d4d7ac20be8159b639
/kaaz <92046563+KaazDW@users.noreply.github.com>
2024-01-26 09:17:28 +0100

    [host] vercel analytics

--- src/main.js
+++ src/main.js
@@ -3,27 +3,9 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-// Importez fetch si ce n'est pas déjà fait
-import 'whatwg-fetch';
+import VercelAnalytics from '@vercel/analytics';
 
-// Fonction pour envoyer un événement à Vercel Analytics
-function sendAnalyticsEvent(eventName, eventData = {}) {
-    fetch(`https://api.vercel.com/v1/record?projectId=prj_c41jkuMu7kZ6v7sbph2hwuVLgX4l&eventName=${eventName}`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer your-vercel-api-token', // Remplacez par votre API token Vercel
-        },
-        body: JSON.stringify(eventData),
-    })
-        .then(response => response.json())
-        .then(data => console.log('Event sent successfully', data))
-        .catch(error => console.error('Error sending event', error));
-}
-
-// Exemple d'utilisation de la fonction pour envoyer un événement
-sendAnalyticsEvent('pageview', { path: '/tracks' });
-
+VercelAnalytics({ projectId: 'prj_LIW8SSAt1NTwMQJWEvVExdY1lejp' });
 
 const app = createApp(App)
 
