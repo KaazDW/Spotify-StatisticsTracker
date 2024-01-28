d83432597453cc844c75c4abff8401b5afcc9054
/kaaz <92046563+KaazDW@users.noreply.github.com>
2024-01-28 01:28:05 +0100

    [host] redirect url correction

--- src/authCodeWithPkce.ts
+++ src/authCodeWithPkce.ts
@@ -7,7 +7,7 @@ export async function redirectToAuthCodeFlow(clientId: string) {
     const params = new URLSearchParams();
     params.append("client_id", clientId);
     params.append("response_type", "code");
-    params.append("redirect_uri", "http://localhost:5173/tracks");
+    params.append("redirect_uri", "https://stats-tracks.vercel.app/tracks");
     params.append("scope", "user-read-private user-read-email user-top-read");
     params.append("code_challenge_method", "S256");
     params.append("code_challenge", challenge);
@@ -23,7 +23,7 @@ export async function getAccessToken(clientId: string, code: string) {
     params.append("client_id", clientId);
     params.append("grant_type", "authorization_code");
     params.append("code", code);
-    params.append("redirect_uri", "http://localhost:5173/tracks");
+    params.append("redirect_uri", "https://stats-tracks.vercel.app/tracks");
     params.append("code_verifier", verifier!);
 
     const result = await fetch("https://accounts.spotify.com/api/token", {
--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,6 @@ import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
-// Obtenez l'URL de déploiement de Vercel
-const vercelUrl = process.env.VERCEL_URL || '';
-
 export default defineConfig({
   plugins: [vue()],
   resolve: {
